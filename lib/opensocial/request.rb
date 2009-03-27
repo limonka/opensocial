f0309fe2fe5918407ef6c09104667653649448c4
Bruce Krysiak & Josh Susser <pair-bruce.krysiak-and-josh.susser@pivotallabs.com>
2009-03-27 16:23:33 -0700

    updated the require...

--- lib/opensocial/request.rb
+++ lib/opensocial/request.rb
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require 'hoptoad-notifier'
+require 'hoptoad_notifier'
 
 module OpenSocial #:nodoc:
 
