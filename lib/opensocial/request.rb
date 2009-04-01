94ed3790a15330c4371e8cdf8277a9732767b190
Chad Woolley & Nate Clark <pair-chad.woolley-and-nate.clark@pivotallabs.com>
2009-04-01 11:37:24 -0700

    removed explicit require to hoptoad_notifier

--- lib/opensocial/request.rb
+++ lib/opensocial/request.rb
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require 'hoptoad_notifier'
-
 module OpenSocial #:nodoc:
 
   # Provides a wrapper for a single request to an OpenSocial endpoint, either
